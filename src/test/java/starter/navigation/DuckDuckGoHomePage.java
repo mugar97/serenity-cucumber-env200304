755cc36ad0f66d0e67672f64eecc5ab9d7bce073
mugar97 <cristhian.murcia@endava.com>
2020-03-04 10:48:29 -0500

    Adding first pipeline

--- src/test/java/starter/navigation/DuckDuckGoHomePage.java
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -3,5 +3,5 @@ package starter.navigation;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://duckduckgo.com")
+@DefaultUrl("page:home.page")
 class DuckDuckGoHomePage extends PageObject {}
